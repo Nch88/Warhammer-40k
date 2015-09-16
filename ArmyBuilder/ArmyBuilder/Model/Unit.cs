8bdc5bd1ae1e1dca935ff697d71f5875a2c3ea26
Nicolai Christensen <nicolai_christensen@hotmail.com>
2015-09-16 23:41:07 +0200

    Created project, added first couple of classes.

---
+++ ArmyBuilder/ArmyBuilder/Model/Unit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmyBuilder.Model
+{
+    public enum Armies
+    {
+        Necrons
+    }
+
+    public class Unit
+    {
+        public Armies Army { get; set; }
+        public string Name { get; set; }
+
+        //Attributes
+        public int WeaponSkill { get; set; }
+        public int BalisticSkill { get; set; }
+        public int Toughness { get; set; }
+        public int Strength { get; set; }
+        public int Wounds { get; set; }
+        public int Attacks { get; set; }
+        public int Initiative { get; set; }
+        public int Leadership { get; set; }
+
+        public Tuple<int, int> UnitMinMaxSize { get; set; }
+        public int PointsPerModel { get; set; }
+    }
+
+    public class SelectedUnit : Unit
+    {
+        public int UnitSize { get; set; }
+
+        public int PointCost { get; set; }
+    }
+}
---
+++ ArmyBuilder/ArmyBuilder/Model/Upgrade.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmyBuilder.Model
+{
+    public class Upgrade
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int PointsPerModel { get; set; }
+
+        public Tuple<int, int> UpgradeMinMaxCount { get; set; }
+    }
+
+    public class SelectedUpgrade : Upgrade
+    {
+        public int UpgradedUnits { get; set; }
+    }
+}
